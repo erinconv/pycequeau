456ce5ec58c635dfedd9c93728b783d47bfa13a3
HabibaFerchichi <149753436+HabibaFerchichi@users.noreply.github.com>
2025-06-16 15:31:21 -0400

    Update carreauxEntiers.py
    did not work with separated function, use instead mean_canopy function in CPfishnet

--- src/physiographic/carreauxEntiers.py
+++ src/physiographic/carreauxEntiers.py
@@ -65,16 +65,3 @@ def ComputeMeanSlope(gdf: gpd.GeoDataFrame,
     # gdf["meanSlope"] = 
     return stat_array
 
-def ComputeMeanCanopy(gdf: gpd.GeoDataFrame,
-                      Canopy_name: str,
-                      attr: str) -> np.ndarray:
-    gdf = gdf.sort_values(by=attr)
-    zonal_stats = rs.zonal_stats(gdf.geometry,
-                                 Canopy_name,
-                                 stats=["mean"])
-    stat_canopy_array = np.zeros(len(zonal_stats))
-    for i in range(len(zonal_stats)):
-        stat_canopy_array[i] = zonal_stats[i]["mean"]
-
-    # gdf["meanCanopy"] = 
-    return  stat_canopy_array
