cf5708d90ae0bd803430209315e2632ebdc39a94
Eisinhower Rincon <e.rinconvar@gmail.com>
2026-03-04 18:53:38 -0500

    Removed matlab code

--- matlab/functions/create_grid.m
+++
@@ -1,9 +0,0 @@
-function grid = create_grid()
-    grid.tMax = [];
-    grid.tMin = [];
-    grid.pTot = [];
-    grid.rayonnement = [];
-    grid.nebulosite = [];
-    grid.pression = [];
-    grid.vitesseVent = [];
-end
