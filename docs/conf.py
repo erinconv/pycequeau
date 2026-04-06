9f26ebe10f5ce4d8fee5bb9134e2635e7f54273e
Eisinhower Rincon <e.rinconvar@gmail.com>
2026-04-06 12:24:12 -0400

    Switch documentation theme to Furo
    Replace the Read the Docs theme with Furo in the local Sphinx configuration, the docs development dependencies, and the Read the Docs conda environment so local builds and hosted RTD builds use the same modern theme stack.

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,11 @@ autodoc_mock_imports = [
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
+
+html_theme_options = {
+    "navigation_with_keys": True,
+}
 
 # html_static_path = ['_static']
 
